06efe33aec2de947c890b644e9316badcbe003d1
gwinax <gwinax@gamil.com>
2021-10-19 15:03:33 +0800

    fix unused

--- cmd/web/handlers/admin/controller/manager.go
+++ cmd/web/handlers/admin/controller/manager.go
@@ -16,23 +16,24 @@ func NewManager() *Manager {
 		Repo: adminrepo.NewAdminUserRepository(),
 	}
 }
-func (g *Manager) getTest() {
-	where := make(map[string]interface{})
-	where["id"] = 1
-	users1 := g.Repo.Select(where)
-	_, err := g.Ctx.JSON(apgs.ApiReturn(0, "123123", users1))
-	if err != nil {
-		return
-	}
-	//users, _ := g.Repo.SelectById("select * from wk_admin_iser where id=?", 1)
-	//fmt.Println(users)
-	//fmt.Println(users1)
-	//
-	//_, _ = g.Ctx.JSON(apgs.ApiReturn(0, "123123", apgs.Map{
-	//	"Users":  users,
-	//	"Users1": users1,
-	//}))
-}
+
+//func (g *Manager) getTest() {
+//	where := make(map[string]interface{})
+//	where["id"] = 1
+//	users1 := g.Repo.Select(where)
+//	_, err := g.Ctx.JSON(apgs.ApiReturn(0, "123123", users1))
+//	if err != nil {
+//		return
+//	}
+//	//users, _ := g.Repo.SelectById("select * from wk_admin_iser where id=?", 1)
+//	//fmt.Println(users)
+//	//fmt.Println(users1)
+//	//
+//	//_, _ = g.Ctx.JSON(apgs.ApiReturn(0, "123123", apgs.Map{
+//	//	"Users":  users,
+//	//	"Users1": users1,
+//	//}))
+//}
 
 func (g *Manager) Get() {
 	_, _ = g.Ctx.JSON(apgs.ApiReturn(0, "123123", nil))
