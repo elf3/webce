f3598d3525f3fce14b7097fc538807de6d15dd30
gwinax <gwinax@gamil.com>
2021-11-12 16:59:59 +0800

    fix resp

--- cmd/web/handlers/admin/login/login.go
+++ cmd/web/handlers/admin/login/login.go
@@ -10,7 +10,6 @@ import (
 	"webce/cmd/web/handlers/admin/login/request"
 	"webce/pkg/library/jwt"
 	"webce/pkg/library/log"
-	"webce/pkg/library/resp"
 )
 
 // HandlerLogin 登陆Handler
@@ -28,44 +27,44 @@ func (h *HandlerLogin) PostLogin(ctx iris.Context) {
 	req := request.ReqLogin{}
 	err := ctx.ReadForm(&req)
 	if err != nil {
-		resp.Api(ctx, resp.ApiReturn(400, "invalid request", nil))
+		h.Error(400, "invalid request")
 		return
 	}
 	valid := validator.New()
 	err = valid.Struct(req)
 	if err != nil {
 		log.Log.Error("login invalid request data: ", err)
-		resp.Api(ctx, resp.ApiReturn(500, "invalid request data", nil))
+		h.Error(500, "invalid request data")
 		return
 	}
 
 	model := auth.AdminAuth{}
 	login, err := model.Login(req.Username, req.Password, ctx.RemoteAddr())
 	if err != nil {
-		resp.Api(ctx, resp.ApiReturn(500, "invalid login", nil))
+		h.Error(500, "invalid login")
 		return
 	}
-	resp.Api(ctx, resp.ApiReturn(0, "", login))
+	h.Success(login)
 }
 
 // PostRefreshToken  刷新token
 func (h *HandlerLogin) PostRefreshToken(ctx iris.Context) {
 	token := ctx.GetHeader("token")
 	if token == "" {
-		resp.Api(ctx, resp.ApiReturn(400, "invalid request", nil))
+		h.Error(500, "invalid request")
 		return
 	}
 	// 解析token
 	claims, err := jwt.ParseToken(token)
 	if err != nil {
 		log.Log.Error("parse jwt token err:", err)
-		resp.Api(ctx, resp.ApiReturn(500, "invalid request data", nil))
+		h.Error(500, "invalid request data")
 		return
 	}
 	// 判定token中是否存在userid
 	if _, ok := claims["userId"]; !ok {
 		log.Log.Error("parse jwt token exists userId")
-		resp.Api(ctx, resp.ApiReturn(500, "invalid request data", nil))
+		h.Error(500, "invalid request data")
 		return
 	}
 
@@ -75,8 +74,9 @@ func (h *HandlerLogin) PostRefreshToken(ctx iris.Context) {
 	token, err = jwt.CreateToken(strconv.FormatUint(claims["userId"].(uint64), 10), fmt.Sprintf("%v", claims["username"]))
 	if err != nil {
 		log.Log.Error("create jwt token err:", err)
-		resp.Api(ctx, resp.ApiReturn(500, "refreshToken err", nil))
+		h.Error(500, "refreshToken err")
+		return
 	}
 
-	resp.Api(ctx, resp.ApiReturn(0, "", token))
+	h.Success(token)
 }
